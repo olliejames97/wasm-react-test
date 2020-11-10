d388e5a06c8142fc7dcb79f2769987ce28dc84d9
im-OJ <ollie2997@gmail.com>
2020-11-10 18:41:10 +0000

    Use wasm add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
+import waApi from "./wa-api";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <p>Hello World</p>;
+  const [api, setApi] = useState<any>();
+
+  useEffect(() => {
+    waApi.then((api) => {
+      setApi(api.exports);
+    });
+  }, []);
+
+  return (
+    <p>Hello World {api && api.add ? api.add(5, 8) : JSON.stringify(api)}</p>
+  );
 }
 
 export default App;
